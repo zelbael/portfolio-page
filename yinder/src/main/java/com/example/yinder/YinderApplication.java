0ea064ce26952e374959bd76a7e85a5f638e8797
Reiczel Bael <reiczelbael.dev@gmail.com>
2023-12-01 14:34:22 -0800

    updated app properties

---
+++ yinder/src/main/java/com/example/yinder/YinderApplication.java
@@ -0,0 +1,13 @@
+package com.example.yinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YinderApplication.class, args);
+	}
+
+}
